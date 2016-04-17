f75fd6caae49bacc4fc4a03db73826ff42a098d1
Aakash Japi <aakashjapi@gmail.com>
2016-04-16 17:30:53 -0700

    more caching code

--- matrix_benchmarker/gpu_cache.cpp
+++ matrix_benchmarker/gpu_cache.cpp
@@ -1,5 +1,7 @@
 // gpu_cache.cpp
 #include "gpu_cache.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 GPU_Cache::GPU_Cache(int size) {
     max_size = size;
@@ -12,19 +14,41 @@ int GPU_Cache::dim_to_key(int m, int n) {
 bool GPU_Cache::get(int m, int n, void **cpu_ptr, void **gpu_ptr) {
     int key = dim_to_key(m, n);
     if (cache_map.count(key) > 0) {
-        cache_block *found = cache_map[key];
-        *gpu_ptr = found->gpu_ptr;
-        *cpu_ptr = found->cpu_ptr;
+        cache_block found = cache_map[key];
+        *gpu_ptr = found.gpu_ptr;
+        *cpu_ptr = found.cpu_ptr;
         return true;
     } else {
         return false;
     }
 }
 
+bool GPU_Cache::put_and_malloc(int m, int n, void **cpu_ptr, void **gpu_ptr) {
+    
+    if ((cudaMalloc((void **) gpu_ptr, m*n*sizeof(float)) != cudaSuccess)) {
+        printf("error cuda mallocing\n");
+        exit(1);
+    }
+    cudaMemcpy(gpu_ptr, A, m*n*sizeof(float), cudaMemcpyHostToDevice);
+    *cpu_ptr = (float *) malloc(m*n*sizeof(float));
+    FILE *f = fopen(filename, "r");
+    if (fread((void *) A, m*n*sizeof(float), 1,f) == 0) {
+        return false;
+    }
+    fclose(f);
+    return put(m, n, cpu_ptr, gpu_ptr);
+}
+
 bool GPU_Cache::put(int m, int n, void **cpu_ptr, void **gpu_ptr) {
+    cache_block *curr = (*cache_block)malloc(sizeof(cache_block));
+    curr->gpu_ptr = *gpu_ptr;
+    curr->cpu_ptr = *cpu_ptr;
+    curr->m = m;
+    curr->n = n;
+    cache_map[dim_to_key(m, n)] = *curr;
     return true;
 }
 
-bool GPU_Cache::put_and_malloc(int m, int n, void **cpu_ptr, void **gpu_ptr) {
-    return true;
+int main (int argc, char **argv) {
+    return 0;
 }
--- matrix_benchmarker/gpu_cache.h
+++ matrix_benchmarker/gpu_cache.h
@@ -1,6 +1,6 @@
 // gpu_cache.h
-#ifndef gpu_cache_h
-#define gpu_cache_h
+// #ifndef gpu_cache_h
+// #define gpu_cache_h
 
 #include <unordered_map> 
 #include <iostream>
@@ -8,17 +8,21 @@
 
 struct cache_block {
     int m, n;
-    void * cpu_ptr, gpu_ptr;
+    void *cpu_ptr, *gpu_ptr;
 };
 
 class GPU_Cache {
     int max_size;
     int total_size;
-    unordered_map<int, cache_block> cache_map;
+    std::unordered_map<int, cache_block> cache_map;
     
 public:
-    bool get(int m, int n, void *cpu_ptr, void *gpu_ptr);
-    bool put(int m, int n, void *cpu_ptr, void *gpu_ptr);
-    bool put_and_malloc(int m, int n, void *cpu_ptr, void *gpu_ptr);
+	GPU_Cache(int size);
+    bool get(int m, int n, void **cpu_ptr, void **gpu_ptr);
+    bool put(int m, int n, void **cpu_ptr, void **gpu_ptr);
+    bool put_and_malloc(int m, int n, void **cpu_ptr, void **gpu_ptr);
+
+private:
+	int dim_to_key(int m, int n);
 
 };

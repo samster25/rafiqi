933170eca949a0f8077e1d7d136f3dbc3d79e702
Sammy Sidhu <sammy.sidhu@gmail.com>
2016-04-29 19:02:32 -0700

    refactored cpp interface for batching

--- caffe_rest_engine/cpp/classification.h
+++ caffe_rest_engine/cpp/classification.h
@@ -11,7 +11,7 @@ typedef void * c_mat;
 void classifier_init();
 c_model model_init(char*, char*, char*, char*);
 c_mat make_mat(char *, size_t);
-const char* model_classify(c_model, char*, size_t);
+const char* model_classify(c_model model, c_mat c_img);
 const char** model_classify_batch(c_model model, c_mat* c_imgs, int num);
 void model_destroy(c_model);
 

86d84b65bfbbf0181ebfbfe096a9a21f4f568fa6
Jordon Wing <jordonwing@berkeley.edu>
2016-05-01 17:25:37 -0700

    Added simple log parser in utils.

---
+++ caffe_rest_engine/utils/log_parser.py
@@ -0,0 +1,19 @@
+import re
+
+def get_entries(regex, debug_file="../logs/debug.log"):
+    results = []
+    with open(debug_file) as f:
+        for line in f:
+            result = re.search(regex, line)
+            if result is not None:
+                results.append(result.groups())
+    return results
+
+def get_request_times(debug_file="../logs/debug.log"):
+    reg = r'Request returning success took.*?([0-9]+)ms'
+    entries = get_entries(reg, debug_file)
+    return map(lambda i: int(i[0]), entries)
+
+
+if __name__ == "__main__":
+    print get_request_times()

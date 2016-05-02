092c6e0e0ca910749875d0e68bd7d870b2467100
Aakash Japi <aakashjapi@gmail.com>
2016-05-01 17:10:13 -0700

    added scripts to easily register models

---
+++ caffe_rest_engine/utils/register_alexnet.sh
@@ -0,0 +1,1 @@
+./register_model.py alexnet https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel --means_path $1/data/ilsvrc12/imagenet_mean.binaryproto --labels_path $1/data/ilsvrc12/synset_words.txt

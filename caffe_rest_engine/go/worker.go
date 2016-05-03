dffef720cebdb250bfbf3791d8232b5b386ef4ca
Jordon Wing <jordonwing@berkeley.edu>
2016-05-02 21:56:40 -0700

    Remove ref to c_mat

--- caffe_rest_engine/go/worker.go
+++ caffe_rest_engine/go/worker.go
@@ -115,7 +115,7 @@ func (w Worker) classify(job_model string, jobs []Job) []string {
 	//	batch_mats[i] = job.Image
 	//}
 	cstr_arr, err := C.model_classify(
-		(*C.c_mat)(unsafe.Pointer(&jobs[0].Image[0])),
+		(*C.char)(unsafe.Pointer(&jobs[0].Image[0])),
 		C.int(len(jobs[0].Image)),
 	)
 	LogTimef("%v model_classify", start, jobs[0].Model)
